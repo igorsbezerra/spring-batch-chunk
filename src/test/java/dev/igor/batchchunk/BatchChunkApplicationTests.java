35c620b4b9a6b6723a07db38b7ca008fe2b1b04b
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-07-18 20:36:04 -0300

    feat: start project

---
+++ src/test/java/dev/igor/batchchunk/BatchChunkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.igor.batchchunk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchChunkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
